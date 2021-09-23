07ca24b29d83dcd255e8278e15879a3b2435053b
Anderson Nunes de Souza <62269065+AndersonSouza88@users.noreply.github.com>
2021-09-23 00:16:31 -0300

    Add files via upload

--- php/index.php
+++ php/index.php
@@ -9,7 +9,7 @@ include('conecta.php');
   <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Bootstrap 101 Template</title>
+    <title>Teste PHP Anderson Souza</title>
     <link href="bootstrap/css/bootstrap.css" rel="stylesheet">
   </head>
   </head>
